7a6df75a3ec2f4ae37e0d0a5a508d63f7a187e81
Kuldeep <kuldeep363rawat@gmail.com>
2026-01-16 10:03:29 +0530

    Subset Recursion

---
+++ DSA/Subsets_Recursion.js
@@ -0,0 +1,32 @@
+/*
+    Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+    The solution set must not contain duplicate subsets. Return the solution in any order.
+
+    Example 1:
+    Input: nums = [1,2,3]
+    Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+    Example 2:
+    Input: nums = [0]
+    Output: [[],[0]]
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const result = []
+    const find = (i, curr)=>{
+        if(i>=nums.length) {
+            result.push(curr);
+            return;
+        }
+        find(i+1, [...curr,nums[i]])
+        find(i+1, curr)
+    }
+    find(0,[]);
+    return result;
+};
