11d27c61f56784a094dad355c894efd47fd86c9f
Kuldeep <kuldeep363rawat@gmail.com>
2026-03-06 17:25:51 +0530

    Longest Repeating Character Replacement

---
+++ DSA/Longest_Repeating_Character_Replacement.js
@@ -0,0 +1,47 @@
+/**
+    You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+    Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+    
+
+    Example 1:
+
+    Input: s = "ABAB", k = 2
+    Output: 4
+    Explanation: Replace the two 'A's with two 'B's or vice versa.
+    Example 2:
+
+    Input: s = "AABABBA", k = 1
+    Output: 4
+    Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+    The substring "BBBB" has the longest repeating letters, which is 4.
+    There may exists other ways to achieve this answer too.
+    
+
+    Constraints:
+
+    1 <= s.length <= 105
+    s consists of only uppercase English letters.
+    0 <= k <= s.length
+ */
+
+    /**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let start = 0, end = 0, maxFreq = 0, result = 0, freqMap = new Map();
+    while(end<s.length){
+        freqMap.set(s[end], (freqMap.get(s[end]) || 0)+1);
+        maxFreq = Math.max(maxFreq,freqMap.get(s[end]));
+        if(end-start+1-maxFreq <=k) result = Math.max(result,end-start+1)
+        else{
+            freqMap.set(s[start], freqMap.get(s[start])-1);
+            start++;
+        }
+        end++;
+    }
+    return result;
+};
